76df2c897d6c83888e2327f6dc4a10b6fde1496c
lh <lhsmilee@163.com>
2022-03-24 16:21:19 +0800

    pull 提交

--- src/main/java/com/lh/testGit/GitTest.java
+++ src/main/java/com/lh/testGit/GitTest.java
@@ -7,5 +7,6 @@ public class GitTest {
         System.out.println("third");
         System.out.println("master");
         System.out.println("hot-fix");
+        System.out.println("pull-test");
     }
 }
